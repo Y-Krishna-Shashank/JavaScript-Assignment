b17e50c772eb1eb938522893e7974d620fee7f7b
Y-Krishna-Shashank <94391991+Y-Krishna-Shashank@users.noreply.github.com>
2021-12-14 10:44:17 +0530

    submitted assignment-1

---
+++ assignment-1.1.js
@@ -0,0 +1,9 @@
+function example(greeting, name)
+{
+    console.log(greeting(),name);
+}
+function greeting()
+{
+    return "Hello,"
+}
+example(greeting,"Krishna Shashank");
---
+++ assignment-1.2.js
@@ -0,0 +1,5 @@
+nameParser=(firstName,lastName)=>{
+    return firstName[0]+lastName[0];
+}
+
+console.log(nameParser("Roger","Shashank"));
